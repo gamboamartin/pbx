f066a28dad5460bc1fa7bef8896bde7c185a5f99
Ivan Avelar <ivan_avelar6@hotmail.com>
2023-02-27 21:32:06 -0600

    v1.3.0 Se inicializa CRUD Agente

---
+++ orm/pbx_agent.php
@@ -0,0 +1,26 @@
+<?php
+namespace gamboamartin\pbx\models;
+
+use base\orm\_modelo_parent;
+use PDO;
+
+class pbx_agent extends _modelo_parent {
+    public function __construct(PDO $link){
+        $tabla = 'pbx_agent';
+        $columnas = array($tabla=>false);
+        $campos_obligatorios[] = 'codigo';
+        $campos_obligatorios[] = 'descripcion';
+        $campos_obligatorios[] = 'type';
+        $campos_obligatorios[] = 'number';
+        $campos_obligatorios[] = 'name';
+        $campos_obligatorios[] = 'password';
+        $campos_obligatorios[] = 'estatus';
+        $campos_obligatorios[] = 'eccp_password';
+
+        parent::__construct(link: $link,tabla:  $tabla, campos_obligatorios: $campos_obligatorios,
+            columnas: $columnas);
+        $this->NAMESPACE = __NAMESPACE__;
+
+        $this->etiqueta = 'agent';
+    }
+}
---
+++ templates/inputs/pbx_agent/alta.php
@@ -0,0 +1,12 @@
+<?php /** @var gamboamartin\pbx\controllers\controlador_pbx_form $controlador  controlador en ejecucion */ ?>
+<?php use config\views; ?>
+<?php echo $controlador->inputs->type; ?>
+<?php echo $controlador->inputs->number; ?>
+<?php echo $controlador->inputs->name; ?>
+<?php echo $controlador->inputs->password; ?>
+<?php echo $controlador->inputs->eccp_password; ?>
+<?php echo $controlador->inputs->estatus; ?>
+<?php include (new views())->ruta_templates.'botons/submit/alta_bd.php';?>
+<div class="error"></div>
+
+
