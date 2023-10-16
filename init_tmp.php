7083e0e884690c543bdd2fa09f9de2f0ffd9285a
MauricioHdz <mauricioisraelhernandez@gmail.com>
2023-10-16 11:08:18 -0600

    v1.26.0 Se integra actualizacion de paquete

--- init_tmp.php
+++
@@ -1,6 +0,0 @@
-<?php
-require "vendor/autoload.php";
-
-$path_tmp = (new \config\generales())->path_base.'/archivos/tmp';
-$del = (new \gamboamartin\plugins\files())->rmdir_recursive(dir: $path_tmp, mismo: true);
-print_r($del);
